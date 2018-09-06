0e788010a80e8c5c648fbf6a2e71cecadfe43ff1
Steven Kirk <grokys@users.noreply.github.com>
2018-09-06 13:30:47 +0200

    Fix indentation.

--- scripts/modules.ps1
+++ scripts/modules.ps1
@@ -105,7 +105,7 @@ New-Module -ScriptBlock {
         elseif (Test-Path "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe") {
             $msbuild = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
         }
-           elseif (Test-Path "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe") {
+        elseif (Test-Path "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe") {
             $msbuild = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe"
         }
         else {
